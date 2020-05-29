25082612c8a7e4909c9c650c07432ec620661d15
Mat Wilmot <matwilmot90@gmail.com>
2020-05-29 14:41:50 -0700

    Added all inquirer questions with conditionals

--- index.js
+++ index.js
@@ -25,6 +25,32 @@ inquirer
       message: "Enter project description",
       name: "desc",
     },
+    {
+      type: "input",
+      message: "How is this project installed? (leave blank if N/A)",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "What is the intended use of this project?",
+      name: "usage",
+    },
+    {
+      type: "confirm",
+      message: "Is there any licensing that needs to be listed?",
+      name: "licenseQuery",
+    },
+    {
+      type: "input",
+      message: "Enter all licenses to add",
+      name: "licenseRes",
+      when: (answers) => answers.licenseQuery === true,
+    },
+    {
+      type: "confirm",
+      message: "Are there any contributors to credit?",
+      name: "contributors",
+    },
   ])
   .then(function (res) {
     console.log(res.title, res.desc);
